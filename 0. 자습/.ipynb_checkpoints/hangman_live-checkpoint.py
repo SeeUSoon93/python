ef4322eae868b7293b804d996f2b62044d4b3f76
김군순 <rlarnstns@gmail.com>
2023-08-14 12:48:06 +0900

    2

---
+++ 0. 자습/.ipynb_checkpoints/hangman_live-checkpoint.py
@@ -0,0 +1,90 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "3a3b8dec",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "hangman_live = ['''\n",
+    "  +---+\n",
+    "  |   |\n",
+    "  O   |\n",
+    " /|\\  |\n",
+    " / \\  |\n",
+    "      |\n",
+    "=========\n",
+    "''', '''\n",
+    "  +---+\n",
+    "  |   |\n",
+    "  O   |\n",
+    " /|\\  |\n",
+    " /    |\n",
+    "      |\n",
+    "=========\n",
+    "''', '''\n",
+    "  +---+\n",
+    "  |   |\n",
+    "  O   |\n",
+    " /|\\  |\n",
+    "      |\n",
+    "      |\n",
+    "=========\n",
+    "''', '''\n",
+    "  +---+\n",
+    "  |   |\n",
+    "  O   |\n",
+    " /|   |\n",
+    "      |\n",
+    "      |\n",
+    "=========''', '''\n",
+    "  +---+\n",
+    "  |   |\n",
+    "  O   |\n",
+    "  |   |\n",
+    "      |\n",
+    "      |\n",
+    "=========\n",
+    "''', '''\n",
+    "  +---+\n",
+    "  |   |\n",
+    "  O   |\n",
+    "      |\n",
+    "      |\n",
+    "      |\n",
+    "=========\n",
+    "''', '''\n",
+    "  +---+\n",
+    "  |   |\n",
+    "      |\n",
+    "      |\n",
+    "      |\n",
+    "      |\n",
+    "=========\n",
+    "''']"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
